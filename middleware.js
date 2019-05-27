5cd2c147e452b023f39d3620fffd95b4ebcc10f0
Christopher LaPointe <cmlapointe11@gmail.com>
2019-05-27 14:12:41 -0400

    Allow overriding router url on middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const RouterSignature = require('./signature');
+const config = require('./config');
 
 module.exports = {
 
@@ -7,12 +8,13 @@ module.exports = {
    *
    * @param  {string} appDomain - Expected app domain of the JWT to pass validation (usually the domain the request is sent o)
    * @param  {object} additionalVerifyOpts - additional options to pass verifier
+   * @param  {string} [routerUrl=config.ROUTER_URL] Optional override for router url
    * @return {expressMiddleware} Returns a new middleware to be used in express
    */
-  validateSignature(appDomain, additionalVerifyOpts = {}) {
+  validateSignature(appDomain, additionalVerifyOpts = {}, routerUrl = config.ROUTER_URL) {
     if (!appDomain) throw new Error('appDomain arg is required for safe signature validation');
 
-    const signatureValidator = new RouterSignature(Object.assign({ audience: appDomain }, additionalVerifyOpts));
+    const signatureValidator = new RouterSignature(Object.assign({ audience: appDomain }, additionalVerifyOpts), routerUrl);
 
     return (req, res, next) => {
       signatureValidator.assertValidJwt(req.get('X-Router-Signature'))
